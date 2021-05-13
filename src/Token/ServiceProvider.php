cdffd2411e83a10fdb7501d730c261c10a90e3b2
唐启翔 <17297043@qq.com>
2021-05-13 16:48:18 +0800

    Update ServiceProvider.php

--- src/Token/ServiceProvider.php
+++ src/Token/ServiceProvider.php
@@ -20,7 +20,7 @@ class ServiceProvider implements ServiceProviderInterface
     {
         $pimple['access_token'] = function ($pimple) {
             $config = $pimple->getConfig();
-            return new AccessToken($config['mch_id'], $config['appId'], $config['appSecret']);
+            return new AccessToken($config['mch_id'], $config['appId'], $config['appSecret'], $config['Inner']);
         };
     }
 }
